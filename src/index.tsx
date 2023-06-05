752fb444732a6a0b6dfcdc3047e2b23911caf411
Jurand76 <jurand@interia.pl>
2023-06-06 01:11:05 +0200

    feat: tries to publish via CDK

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "~/components/App/App";
+import App from "./components/App/App";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { theme } from "~/theme";
+import { theme } from "./theme";
 
 const queryClient = new QueryClient({
   defaultOptions: {
